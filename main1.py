f0b850fa0e1432d5e748af8c948c78ccd74f63c1
irina <irishka.klimovich.2002@mail.ru>
2021-06-24 14:44:30 +0300

    another solve

--- main1.py
+++ main1.py
@@ -1,50 +1,32 @@
-import math
+def c_gen(n, m):
 
+    rang = [i for i in range(n)]
+    start = rang[0 : m] # first combination
+    end = rang[n - m : n] # last combination
 
-def gen(n, k):
-    arr = [0] * n
+    comb = start
 
-    def rec(i, kk):
-        if i == n:
-            if kk == k:
-                yield arr
+    while True:
+        yield comb
+        pos = m - 1
+        while True:
+            if comb[pos] == end[pos]:
+                pos -= 1
+
+            else:
+                break
+
+        comb[pos] += 1
+        for i in range(pos + 1, m, +1):
+            comb[i] = comb[i - 1] + 1
+
+        if comb == end:
+            yield comb
             return
-        if kk + 1 <= k:
-            arr[i] = 1
-            yield from rec(i + 1, kk + 1)
-        arr[i] = 0
-        yield from rec(i + 1, kk)
-
-    yield from rec(0, 0)
 
 
-def C(n, k):
-    return int(math.factorial(n) / (math.factorial(n - k) * math.factorial(k)))
-
-
-def get_nth(n, k, nth):
-    ans = []
-    kk = 0
-    for i in range(0, n):
-        c = C(n - i - 1, k - kk - 1)
-        if nth <= c:
-            ans.append(1)
-            kk += 1
-        else:
-            nth -= c
-            ans.append(0)
-
-        if kk == k:
-            break
-    while len(ans) < n:
-        ans.append(0)
-    return ans
-
-
-def gen2(n, k):
-    for i in range(C(n, k)):
-        yield get_nth(n, k, i + 1)
-
-
-for gg in gen2(3, 1):
-    print(gg)
+if __name__ == '__main__':
+    n = 5
+    m = 3
+    for el in c_gen(n, m):
+        print(el)
